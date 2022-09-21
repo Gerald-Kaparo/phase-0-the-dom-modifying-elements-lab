2f7435d5fb6537015c86e287f2b194a65bd202e1
Gerald <kapgerado05@gmail.com>
2022-09-21 15:34:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Write your code here!
 let  newHeader= document.getElementById("victory");
+const element = document.createElement("div")
+document.body.append(element);
+
+
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+  }
